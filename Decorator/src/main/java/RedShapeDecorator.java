428eea61927735848ceffa6cc73e505801db446d
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-01-11 01:05:07 +0530

    composite

---
+++ Decorator/src/main/java/RedShapeDecorator.java
@@ -0,0 +1,15 @@
+public class RedShapeDecorator extends ShapeDecorator {
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder();
+    }
+
+    private void setRedBorder() {
+        System.out.println("Border Color: Red");
+    }
+}
